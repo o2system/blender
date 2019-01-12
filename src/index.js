f3324c5ab1e21914d8dcd80c9358070715b0cf68
Nur Wachid <wachid@outlook.com>
2019-01-12 08:50:13 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-require("./loaders");
-require("./plugins");
+/**
+ * This file is part of the O2System Framework package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author         Steeve Andrian Salim
+ * @copyright      Copyright (c) Steeve Andrian Salim
+ */
+// ------------------------------------------------------------------------
+
+/**
+ * Class Blender
+ */
+export default class Blender {
+    constructor() {
+        console.log('Hello Iam blender');
+    }
+}
